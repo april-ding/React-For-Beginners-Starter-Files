e51a364d13b16d5089215ec1a0b02131799f3728
April Ding <apr.ding@gmail.com>
2021-08-03 23:55:38 +0800

    restructure to implement router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react'; // ES6 Modules
 import { render } from 'react-dom'; // only need the render() method from ReactDOM
+import Router from './components/Router';
 
-import StorePicker from './components/StorePicker';
-import App from './components/App';
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
